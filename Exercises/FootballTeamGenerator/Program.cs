882a66a455a6de18c0cf086b32d57a0c4c29be53
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-07-21 15:52:09 +0300

    Add FootballTeamGenerator

---
+++ Exercises/FootballTeamGenerator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FootballTeamGenerator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Team> teams = new List<Team>();
+			string input = Console.ReadLine();
+			while(input != "END")
+			{
+				List<string> data = input.Split(';').ToList();
+				try
+				{
+					switch (data[0])
+					{
+						case "Team":
+							teams.Add(new Team(data[1]));
+							break;
+
+						case "Add":
+							if(teams.Any(x => x.Name == data[1]))
+							{
+								Team team = teams.Find(x => x.Name == data[1]);
+								team.Players.Add(new Player(data[2], int.Parse(data[3]), int.Parse(data[4]), int.Parse(data[5]), int.Parse(data[6]), int.Parse(data[7])));
+							}
+							else
+							{
+								throw new Exception($"Team {data[1]} does not exist.");
+							}
+							break;
+
+						case "Remove":
+							teams.Find(x => x.Name == data[1]).RemovePlayer(data[2]);
+							break;
+
+						case "Rating":
+							if (teams.Any(x => x.Name == data[1]))
+							{
+								Console.WriteLine($"{data[1]} - {teams.Find(x => x.Name == data[1]).Rating}");
+							}
+							else
+							{
+								throw new Exception($"Team {data[1]} does not exist.");
+							}
+							break;
+					}
+				}
+				catch(Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+				input = Console.ReadLine();
+			}
+		}
+	}
+}
---
+++ Exercises/FootballTeamGenerator/Team.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FootballTeamGenerator
+{
+	public class Team
+	{
+		private string name;
+		private List<Player> players;
+
+		public Team(string name)
+		{
+			this.Name = name;
+			players = new List<Player>();
+		}
+
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new Exception("A name should not be empty.");
+				}
+				else
+				{
+					this.name = value;
+				}
+			}
+		}
+
+		public double Rating
+		{
+			get
+			{
+				if (this.players.Count == 0)
+				{
+					return 0;
+				}
+				else
+				{
+					return Math.Round((this.players.Sum(x => x.SkillLevel) / this.players.Count));
+				}
+			}
+		}
+
+		public List<Player> Players
+		{
+			get
+			{
+				return this.players;
+			}
+			set
+			{
+				this.players = value;
+			}
+		}
+
+		public void RemovePlayer(string name)
+		{
+			if(this.players.Any(x => x.Name == name))
+			{
+				this.players.Remove(this.players.Find(x => x.Name == name));
+			}
+			else
+			{
+				throw new Exception($"Player {name} is not in {this.name} team.");
+			}
+		}
+	}
+}
