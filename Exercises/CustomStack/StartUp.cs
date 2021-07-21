34c0a20599318f74c136dd4fc49b51dc30f92c63
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-07-21 14:17:01 +0300

    Edit StackOfStrings

--- Exercises/CustomStack/StartUp.cs
+++ Exercises/CustomStack/StartUp.cs
@@ -3,33 +3,11 @@ using System.Collections.Generic;
 
 namespace CustomStack
 {
-	class StartUp
+	public class StartUp
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			Console.Write("Hello");
-			Console.Write("World!");
-		}
-	}
-
-	public class StackOfStrings : Stack<string>
-	{
-		public bool IsEmpty()
-		{
-			if(this.Count == 0)
-			{
-				return true;
-			}
-
-			return false;
-		}
-
-		public void AddRange(Stack<string> stack)
-		{
-			foreach(string st in stack)
-			{
-				this.Push(st);
-			}
+			
 		}
 	}
 }
