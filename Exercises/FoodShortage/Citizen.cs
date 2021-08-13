94efe8d11ebf33cb94e58f3e6925c0569b1edc03
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-08-13 20:12:34 +0300

    Add FoodShortage

---
+++ Exercises/FoodShortage/Citizen.cs
@@ -0,0 +1,17 @@
+﻿namespace FoodShortage
+{
+	class Citizen : BirthdayCelebrations.Person, IBuyer
+	{
+		public Citizen(string name, int age, string id, string birthdate) : base(name, age, id, birthdate)
+		{
+			this.Food = 0;
+		}
+
+		public int Food { get; set; }
+
+		public void BuyFood()
+		{
+			this.Food += 10;
+		}
+	}
+}
