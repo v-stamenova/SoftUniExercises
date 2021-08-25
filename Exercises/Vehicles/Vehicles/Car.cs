f1f3ade473f3a40a1a1d2111c50c00d299b8cdd8
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-08-25 23:17:35 +0300

    Vehicle exercise - first part and the extention

---
+++ Exercises/Vehicles/Vehicles/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace Vehicles.Vehicles
+{
+	class Car : Vehicle
+	{
+		public Car(double fuelQuantity, double fuelConsumptionPerKm, double tankCapacity) : base(fuelQuantity, fuelConsumptionPerKm + 0.9, tankCapacity)
+		{
+		}
+
+		public override string Drive(double distance)
+		{
+			if (this.FuelQuantity >= this.FuelConsumptionPerKm * distance)
+			{
+				this.FuelQuantity -= distance * this.FuelConsumptionPerKm;
+				return $"Car travelled {distance} km";
+			}
+
+			return "Car needs refueling";
+		}
+	}
+}
---
+++ Exercises/Vehicles/Vehicles/Truck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Vehicles.Vehicles
+{
+	class Truck : Vehicle
+	{
+		public Truck(double fuelQuantity, double fuelConsumptionPerKm, double tankCapacity) : base(fuelQuantity, fuelConsumptionPerKm + 1.6, tankCapacity)
+		{
+		}
+
+		public override string Drive(double distance)
+		{
+			if (this.FuelQuantity >= this.FuelConsumptionPerKm * distance)
+			{
+				this.FuelQuantity -= distance * this.FuelConsumptionPerKm;
+				return $"Truck travelled {distance} km";
+			}
+
+			return "Truck needs refueling";
+		}
+
+		public override void Refuel(double quantity)
+		{
+			if (quantity <= 0)
+			{
+				throw new ArgumentException("Fuel must be a positive number");
+			}
+			else if (quantity * 0.95 + this.FuelQuantity > this.TankCapacity)
+			{
+				throw new ArgumentException($"Cannot fit {quantity} fuel in the tank");
+			}
+			else
+			{
+				this.FuelQuantity += quantity * 0.95;
+			}
+		}
+	}
+}
