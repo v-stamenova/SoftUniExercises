a681d29c0f2cfa9da31677bdbbaa9d5e2c33d655
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-08-27 18:29:56 +0300

    Add `AppliedArithmetics`

---
+++ Exercises/AppliedArithmetics/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+	class Program
+	{
+		static List<int> numbers;
+
+		static void Main(string[] args)
+		{
+			numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+			string input = Console.ReadLine();
+
+			while (input != "end")
+			{
+				switch (input)
+				{
+					case "add":
+						Add();
+						break;
+					case "multiply":
+						Multiply();
+						break;
+					case "subtract":
+						Subtract();
+						break;
+					case "print":
+						Print();
+						break;
+				}
+				if(input == "end")
+				{
+					break;
+				}
+				input = Console.ReadLine();
+			}
+		}
+
+		static void Add()
+		{
+			numbers = numbers.Select(x => { x++ ; return x; }).ToList();
+		}
+
+		static void Multiply()
+		{
+			numbers = numbers.Select(x => { x = x * 2; return x; }).ToList();
+		}
+
+		static void Subtract()
+		{
+			numbers = numbers.Select(x => { x--; return x; }).ToList();
+		}
+
+		static void Print()
+		{
+			Console.WriteLine(String.Join(' ', numbers));
+		}
+	}
+}
