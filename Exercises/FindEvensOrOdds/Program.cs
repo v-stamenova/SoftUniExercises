696a042dc64febd8dc663658a661c0c861002cd5
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-08-27 18:29:38 +0300

    Add `FindEvensOrOdds`

---
+++ Exercises/FindEvensOrOdds/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindEvensOrOdds
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Predicate<int> isEven = IsEven;
+			List<int> borders = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+			List<int> numbers = new List<int>();
+
+			for(int i = borders[0]; i <= borders[1]; i++)
+			{
+				numbers.Add(i);
+			}
+
+			string oddOrEven = Console.ReadLine();
+			List<int> selectedNumbers;
+			if(oddOrEven == "odd")
+			{
+				selectedNumbers = numbers.Where(x => !isEven(x)).ToList();
+			}
+			else
+			{
+				selectedNumbers = numbers.Where(x => isEven(x)).ToList();
+			}
+
+			Console.WriteLine(string.Join(' ', selectedNumbers));
+		}
+
+		static bool IsEven(int number)
+		{
+			if(number % 2 == 0)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
