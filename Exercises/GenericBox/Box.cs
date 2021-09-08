6403c669da66cd2b52f7fd2dfa93dfb3441c3126
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-09-08 14:33:01 +0300

    Add `GenericBox`

---
+++ Exercises/GenericBox/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBox
+{
+	public class Box<T>
+	{
+		private T value;
+
+		public Box(T value)
+		{
+			this.value = value;
+		}
+
+		public override string ToString()
+		{
+			return $"{value.GetType().FullName}: {value}";
+		}
+	}
+}
