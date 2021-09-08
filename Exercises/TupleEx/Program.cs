67068065b3c254c7e0c5c304f7c15c65ebf74be2
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-09-08 14:33:50 +0300

    Add `TupleEx`

---
+++ Exercises/TupleEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TupleEx
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> input = Console.ReadLine().Split(' ').ToList();
+			Tuple<string, string> firstTuple = new Tuple<string, string>($"{input[0]} {input[1]}", input[2]);
+
+			input = Console.ReadLine().Split(' ').ToList();
+			Tuple<string, int> secondTuple = new Tuple<string, int>(input[0], int.Parse(input[1]));
+
+			input = Console.ReadLine().Split(' ').ToList();
+			Tuple<string, double> thirdTuple = new Tuple<string, double>(input[0], double.Parse(input[1]));
+		}
+	}
+}
---
+++ Exercises/TupleEx/Tuple.cs
@@ -0,0 +1,19 @@
+﻿namespace TupleEx
+{
+	class Tuple<Type1, Type2>
+	{
+		private Type1 item1;
+		private Type2 item2;
+
+		public Tuple(Type1 item1, Type2 item2)
+		{
+			this.item1 = item1;
+			this.item2 = item2;
+		}
+
+		public override string ToString()
+		{
+			return $"{item1} -> {item2}";
+		}
+	}
+}
