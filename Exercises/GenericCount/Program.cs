accb21bfbdefc7a5d574c225520f67b0c8b525fa
Валерия Стаменова <valeria.stamenova3@gmail.com>
2021-09-08 14:33:23 +0300

    Add `GenericCount`

---
+++ Exercises/GenericCount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCount
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int inputLines = int.Parse(Console.ReadLine());
+			List<Box<double>> list = new List<Box<double>>();
+
+			for (int i = 0; i < inputLines; i++)
+			{
+				list.Add(new Box<double>(double.Parse(Console.ReadLine())));
+			}
+
+			Box<double> box = new Box<double>(double.Parse(Console.ReadLine()));
+
+			Console.WriteLine(box.NumberOfElementsGreaterThan(list));
+		}
+	}
+}
